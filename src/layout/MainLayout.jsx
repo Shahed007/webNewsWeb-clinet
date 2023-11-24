4c04e4b3d334a734f18c24760902fd6f93fc7693
Shahed <sk0178397@gmial.com>
2023-11-24 11:40:00 +0600

    Created structure file.

---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../pages/shared/navbar/Navbar";
+
+const MainLayout = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+    </div>
+  );
+};
+
+export default MainLayout;
