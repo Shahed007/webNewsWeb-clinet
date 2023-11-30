1704c57da61c0f15e64e2cde24cc7bf1a4235a01
Shahed <sk0178397@gmial.com>
2023-11-30 16:59:19 +0600

    Solved problem

--- src/components/card/ArticlesCard.jsx
+++ src/components/card/ArticlesCard.jsx
@@ -52,7 +52,7 @@ const ArticlesCard = ({ data, userRoll }) => {
           <h4 className=" text-lg font-bold text-text_primary grow">{title}</h4>
           <p className="mt-3 font-normal text-lg text-text_secondary/80 ">
             {description.slice(0, 60)}...
-            <Button variant="text" size="sm" disabled={userRoll.premiumTaken === "no"}>
+            <Button variant="text" size="sm" disabled={userRoll?.premiumTaken === "no"}>
               <Link to={`/articles-Details/${_id}`}>Read more</Link>
             </Button>
           </p>
